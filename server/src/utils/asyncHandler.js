c8dd8c2d7adb52d4fb3e2a0bf10365c4d654fc55
Abdalla <pioquule@gmail.com>
2026-08-31 20:00:41 +0300

    clean

--- server/src/utils/asyncHandler.js
+++
@@ -1,6 +0,0 @@
-// Wraps async route handlers so thrown errors are forwarded to the error middleware
-const asyncHandler = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next);
-};
-
-export default asyncHandler;
